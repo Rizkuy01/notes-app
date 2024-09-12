1267f5e493ea54e5a3fa9edf2ebbb99daa430384
Rizkuy01 <rizkysaputraa2004.com>
2024-09-12 14:28:53 +0700

    view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React, { useState } from "react";
+import { getInitialData } from "./utilities/data";
+import NoteForm from "./component/NoteForm.jsx";
+import NoteList from "./component/NoteList.jsx";
 
 function App() {
+  const [data, setData] = useState(getInitialData());
+  const [searchData, setSearchData] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const filteredData = data.filter(data =>
+    data.title.toLowerCase().includes(searchData.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <h1>
-        hello world
-      </h1>
+    <div className="container">
+      <div className="header text-center">
+        <h1 className="font-bold text-4xl">Note List</h1>
+      </div>
+
+      <div className="flex justify-end mb-5">
+        <input
+          type="text"
+          className="p-2 border border-gray-300 rounded w-64"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+          placeholder="Search"
+        />
+      </div>
+
+      <NoteList data={filteredData} setData={setData} />
+
+      {isModalOpen && (
+        <NoteForm setNotes={setData} setModalOpen={setIsModalOpen} />
+      )}
     </div>
   );
 }
